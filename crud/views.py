0c9ea4daa59491a27992adca7b7f7a1dcc61f8f8
Diana <diana2monegro1@gmail.com>
2021-11-18 11:20:54 -0400

    Adding Home and Pedido view

--- crud/views.py
+++ crud/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request,'crud/home.html')
+
+def about(request):
+    return render(request,'crud/pedido.html', {'title': 'Pedido'})
