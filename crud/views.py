0c9c13345ca2fe2a7d4cfa56a6dc4570af0f1336
Diana <diana2monegro1@gmail.com>
2021-11-18 11:44:51 -0400

    Adding urls, views and initial html and css files to project

--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return render(request,'crud/home.html')
+    #return HttpResponse('<h1>HOME</h1>')
 
-def about(request):
+def pedido(request):
     return render(request,'crud/pedido.html', {'title': 'Pedido'})
